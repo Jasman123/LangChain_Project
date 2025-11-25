b46447a24f32b4530c1889d88a027e0b916bf282
Jasman <53416589+Jasman123@users.noreply.github.com>
2025-11-25 07:12:03 +0700

    Enhance app.py with summarization and web loading
    Added functionality for conversation summarization and web content loading.

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, Remo
 from typing import Literal
 from IPython.display import Image, display
 from langgraph.checkpoint.memory import MemorySaver
+from langchain_community.document_loaders import WebBaseLoader, AsyncChromiumLoader
+from langchain_community.document_transformers import Html2TextTransformer
+from langchain_text_splitters.character import CharacterTextSplitter
+import asyncio
+import os
+import copy
+os.environ["USER_AGENT"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+
+
+# import requests
 
 
 load_dotenv()
@@ -20,45 +30,108 @@ chat = ChatGoogleGenerativeAI(
 
 
 class State(MessagesState):
-    pass
+    summary:str
+    
+# def filter_messages(state: MessagesState):
+#     delele_messages = [RemoveMessage(id=msg.id) for msg in state['messages'][:-2]]
 
 
 def call_model(state: State):
-    response = chat.invoke(state["messages"])
+
+    summary = state.get("summary", "")
+
+    if summary:
+        system_message = f"You are a helpful assistant! Here is the conversation summary so far: {summary}"
+        
+        messages = [SystemMessage(content=system_message)] + state["messages"]
+
+    else:
+        messages = state["messages"]
+
+
+    response = chat.invoke(messages)
     return {"messages": state["messages"] + [response]}
+
+
+def summarize_conversation(state: State):
+    print("\n=== SUMMARIZING CONVERSATION ===\n")
+    summary = state.get("summary", "")
+
+    if summary:
+        
+        # A summary already exists
+        summary_message = (
+            f"This is a summary of the conversation to date: {summary}\n\n"
+            "Extend the summary by taking into account the new messages above:"
+        )
+        
+    else:
+        summary_message = "Create a summary of the conversation above:"
+
+    messages = state["messages"] + [HumanMessage(content=summary_message)]
+    response = chat.invoke(messages)
+
+    # Delete all but the 2 most recent messages
+    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+    new_messages = state["messages"][-2:]
+    return {"summary": response.content, "messages": delete_messages + new_messages}
+
+def should_summarize(state: State) -> Literal["summarize",END]:
+    """Return the next node to execute."""
+
+    messages = state["messages"]
+
+    if len(messages) > 6:
+        return "summarize"
+    else:
+        return END
+
+
+async def load_website(urls: list[str]) -> list[str]:
+    loader = AsyncChromiumLoader(urls, user_agent="MyAppUserAgent")
+    docs = await loader.aload()
+    html2text = Html2TextTransformer()
+    docs_transformed = html2text.transform_documents(docs)
+    return docs_transformed[0].page_content
 
 builder = StateGraph(State)
 builder.add_node("model", call_model)
+builder.add_node("summarize", summarize_conversation)
+
 
 builder.add_edge(START, "model")
+builder.add_conditional_edges("model", should_summarize)
+builder.add_edge("summarize", END)
 builder.add_edge("model", END)
+
+
 
 graph = builder.compile()
 memory = MemorySaver()
 react_graph_memory = builder.compile(checkpointer=memory)
 config = {"configurable": {"thread_id": "1"}}
 
-messages= [
-    SystemMessage(content="You are a helpful assistant! Your name is Bob."),
-    HumanMessage(content="What is your name?"),
-]
-messages_no_memories = graph.invoke({"messages": messages})
-messages_no_memories = graph.invoke(
-    {"messages": [HumanMessage(content="What is your name again?")]}
+
+result = asyncio.run(load_website(["https://kemendikdasmen.go.id/berita/14190-kemendikdasmen-prioritaskan-pengembangan-konten-bidang-kejur"]))
+# print(result)
+# print(len(result))
+#resul 9519
+string_cleaned = copy.deepcopy(result)
+string_cleaned = string_cleaned.strip().replace("\n"," ").replace("#"," "). replace("*"," ")
+# print(string_cut)
+# print(len(string_cut))
+char_splitter = CharacterTextSplitter(
+    separator=" ",
+    chunk_size=2000,
+    chunk_overlap=200,
 )
 
-messages_w_memories = react_graph_memory.invoke({"messages": messages}, config)
-messages_w_memories = react_graph_memory.invoke(
-    {"messages": [HumanMessage(content="What is your name again?")]},
-    config
-)
+pages_char_split = char_splitter.split_text(string_cleaned)
+print(pages_char_split[-2])
 
 
-print("\n\n===== WITHOUT MEMORIES =====")
-for i in messages_no_memories["messages"]:
-    i.pretty_print()                        
-print("\n\n===== WITH MEMORIES =====")
-for i in messages_w_memories["messages"]:
-    i.pretty_print()    
 
-# print(result["messages"][-1].content)
+
+
+
+
