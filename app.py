e00f16196ef87dc0e7c2fa738990e7b5e6928c5b
Jasman <53416589+Jasman123@users.noreply.github.com>
2025-11-26 08:04:44 +0700

    Refactor PDF loading and vector store functions
    update code read pdf

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain_community.document_loaders import WebBaseLoader, AsyncChromiumLoa
 from langchain_community.document_transformers import Html2TextTransformer
 from langchain_text_splitters.character import CharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
 import asyncio
 import os
 import copy
@@ -30,76 +31,10 @@ chat = ChatGoogleGenerativeAI(
 )
 
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="gemini-2.5-flash",
+    model="text-embedding-004",
     timeout=30,
     max_retries=2,
 )
-
-
-class State(MessagesState):
-    summary:str
-    
-# def filter_messages(state: MessagesState):
-#     delele_messages = [RemoveMessage(id=msg.id) for msg in state['messages'][:-2]]
-
-
-def call_model(state: State):
-
-    summary = state.get("summary", "")
-
-    if summary:
-        system_message = f"You are a helpful assistant! Here is the conversation summary so far: {summary}"
-        
-        messages = [SystemMessage(content=system_message)] + state["messages"]
-
-    else:
-        messages = state["messages"]
-
-
-    response = chat.invoke(messages)
-    return {"messages": state["messages"] + [response]}
-
-
-def summarize_conversation(state: State):
-    print("\n=== SUMMARIZING CONVERSATION ===\n")
-    summary = state.get("summary", "")
-
-    if summary:
-        
-        # A summary already exists
-        summary_message = (
-            f"This is summary of the conversation to date: {summary}\n\n"
-            "Extend the summary by taking into account the new messages above:"
-        )
-        
-    else:
-        summary_message = "Create a summary of the conversation above:"
-
-    messages = state["messages"] + [HumanMessage(content=summary_message)]
-    response = chat.invoke(messages)
-
-    # Delete all but the 2 most recent messages
-    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
-    new_messages = state["messages"][-2:]
-    return {"summary": response.content, "messages": delete_messages + new_messages}
-
-def should_summarize(state: State) -> Literal["summarize",END]:
-    """Return the next node to execute."""
-
-    messages = state["messages"]
-
-    if len(messages) > 6:
-        return "summarize"
-    else:
-        return END
-
-
-async def load_website(urls: list[str]) -> list[str]:
-    loader = AsyncChromiumLoader(urls, user_agent="MyAppUserAgent")
-    docs = await loader.aload()
-    html2text = Html2TextTransformer()
-    docs_transformed = html2text.transform_documents(docs)
-    return docs_transformed[0].page_content
 
 def load_pdf(file_path: str) -> str:
     loader = PyPDFLoader(file_path)
@@ -107,31 +42,46 @@ def load_pdf(file_path: str) -> str:
     for i in range(len(docs)):
         docs[i].page_content =' '.join(docs[i].page_content.split())
 
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=80)
     pages_splitter = text_splitter.split_documents(docs)
-    # full_text = "\n".join([t.page_content for t in texts])
     return pages_splitter
 
-builder = StateGraph(State)
-builder.add_node("model", call_model)
-builder.add_node("summarize", summarize_conversation)
+def build_vector_store(documents):
+    vector_store = Chroma.from_documents(
+        documents,
+        embedding=embeddings,
+        collection_name="pdf_docs",
+        persist_directory="chroma_db"
+    )
+    
+    return vector_store
 
+def load_vector_store(embeddings):
+    vector_store = Chroma(
+        embedding_function=embeddings,
+        collection_name="pdf_docs",
+        persist_directory="chroma_db"
+    )
+    return vector_store
 
-builder.add_edge(START, "model")
-builder.add_conditional_edges("model", should_summarize)
-builder.add_edge("summarize", END)
-builder.add_edge("model", END)
+# document = load_pdf(r"C:\Users\OFFICE-COB\Documents\COB Engineer\LangChain_AI_Project\langchain_project\data_pdf.pdf")
+# vector_store = build_vector_store(document)
 
+vector_store = load_vector_store(embeddings)
+print("Vector store loaded.")
+print("len of collection:", vector_store._collection.count())
+resutl = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k":3, 'lambda_mult': 0.7}
+)
+question = 'What is main topic of this document?'
+retrieved_docs = resutl.invoke(question)
+print("Retrieved Documents:")
+for doc in retrieved_docs:
+    print("---- Document ----")
+    print("page number:", doc.metadata.get('page'))
+    print(doc.page_content)
 
-
-graph = builder.compile()
-memory = MemorySaver()
-react_graph_memory = builder.compile(checkpointer=memory)
-config = {"configurable": {"thread_id": "1"}}
-
-
-document = load_pdf(r"C:\Users\OFFICE-COB\Documents\COB Engineer\LangChain_AI_Project\langchain_project\data_pdf.pdf")
-print(document)
 
 
 
